6f745a21fa7bc900b0fc37c7a416c119d394b92f
Jason Stephen <jason@wireless-10-146-40-237.public.utexas.edu>
2019-02-03 22:51:28 -0600

    first commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Jason Stephen on 2/3/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
