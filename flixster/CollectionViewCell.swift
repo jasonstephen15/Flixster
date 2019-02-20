fc1c822af7bab84c8be3cf67bf045e7143de4530
Jason Stephen <jason@Jasons-MacBook-Pro.local>
2019-02-19 22:25:25 -0600

    tabBar w/ Superhero

---
+++ flixster/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  flixster
+//
+//  Created by Jason Stephen on 2/18/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ flixster/MovieGridViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridViewCell.swift
+//  flixster
+//
+//  Created by Jason Stephen on 2/18/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
